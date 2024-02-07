6043ef044a802dcd4ea0b16e30e951fe5072c79a
Cornelis Malmros <122611844+Vengefulship136@users.noreply.github.com>
2024-02-07 14:56:44 +0100

    SoMuchPainCould'veBeenAvoided
    This is all your fault

--- Assets/Scripts/DiceCustomization.cs
+++ Assets/Scripts/DiceCustomization.cs
@@ -2,6 +2,7 @@ using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DiceCustomization : MonoBehaviour
 {
@@ -30,46 +31,62 @@ public class DiceCustomization : MonoBehaviour
                 Abilities.GetFromName("Block"),
                 Abilities.GetFromName("Block"))
             );
-
-        Ability newAbility = Abilities.GetFromRarity(CardRarity.Common);
-        CustomiseDice(newAbility, dice);
+        
+        RandomiseAbility();
     }
 
 
     void Update()
     {
-        
-    }    
+    
+    }
 
-    public void CustomiseDice(Ability newAbility, Dice dice)
+    public void CustomiseDice(Ability newAbility)
     {
         if (newAbility is PhysicalAbility)
         {
-            GameObject.Find("Side 1").GetComponent<Renderer>().material.color = new Color(128, 0, 0);
-            GameObject.Find("Side 2").GetComponent<Renderer>().material.color = new Color(128, 0, 0);
-            GameObject.Find("Side 3").GetComponent<Renderer>().material.color = new Color(128, 0, 0);
-            GameObject.Find("Side 4").GetComponent<Renderer>().material.color = new Color(128, 0, 0);
-            GameObject.Find("Side 5").GetComponent<Renderer>().material.color = new Color(128, 0, 0);
-            GameObject.Find("Side 6").GetComponent<Renderer>().material.color = new Color(128, 0, 0);
-            GameObject.Find("New ability").GetComponent<Renderer>().material.color = new Color(128, 0, 0);
+            ChangeColor(Color.red);
+            ChangeText("Physical ability");
         }
         if (newAbility is MagicalAbility)
         {
-            GameObject.Find("Side 1").GetComponent<Renderer>().material.color = new Color(0, 128, 0);
-            GameObject.Find("Side 2").GetComponent<Renderer>().material.color = new Color(0, 128, 0);
-            GameObject.Find("Side 3").GetComponent<Renderer>().material.color = new Color(0, 128, 0);
-            GameObject.Find("Side 4").GetComponent<Renderer>().material.color = new Color(0, 128, 0);
-            GameObject.Find("Side 5").GetComponent<Renderer>().material.color = new Color(0, 128, 0);
-            GameObject.Find("Side 6").GetComponent<Renderer>().material.color = new Color(0, 128, 0);
+            ChangeColor(Color.blue);
+            ChangeText("Magical ability");
         }
         if (newAbility is DefensiveAbility)
         {
-            GameObject.Find("Side 1").GetComponent<Renderer>().material.color = new Color(0, 128, 128);
-            GameObject.Find("Side 2").GetComponent<Renderer>().material.color = new Color(0, 128, 128);
-            GameObject.Find("Side 3").GetComponent<Renderer>().material.color = new Color(0, 128, 128);
-            GameObject.Find("Side 4").GetComponent<Renderer>().material.color = new Color(0, 128, 128);
-            GameObject.Find("Side 5").GetComponent<Renderer>().material.color = new Color(0, 128, 128);
-            GameObject.Find("Side 6").GetComponent<Renderer>().material.color = new Color(0, 128, 128);
+            ChangeColor(Color.green);
+            ChangeText("Defensive ability");
         }
+    }
+    private void ChangeColor(Color color)
+    {
+        GameObject.Find("Side 1").GetComponent<Image>().color = color;
+        GameObject.Find("Side 2").GetComponent<Image>().color = color;
+        GameObject.Find("Side 3").GetComponent<Image>().color = color;
+        GameObject.Find("Side 4").GetComponent<Image>().color = color;
+        GameObject.Find("Side 5").GetComponent<Image>().color = color;
+        GameObject.Find("Side 6").GetComponent<Image>().color = color;
+        GameObject.Find("Cancel").GetComponent<Image>().color = Color.white;
+        GameObject.Find("Confirm").GetComponent<Image>().color = Color.white;
+    }
+    private void ChangeText(string text)
+    {
+        GameObject.Find("Side 1").GetComponentInChildren<Text>().text = text;
+        GameObject.Find("Side 2").GetComponentInChildren<Text>().text = text;
+        GameObject.Find("Side 3").GetComponentInChildren<Text>().text = text;
+        GameObject.Find("Side 4").GetComponentInChildren<Text>().text = text;
+        GameObject.Find("Side 5").GetComponentInChildren<Text>().text = text;
+        GameObject.Find("Side 6").GetComponentInChildren<Text>().text = text;
+    }
+    public void RandomiseAbility()
+    {
+        Ability newAbility = Abilities.GetFromRarity(CardRarity.Common);
+        CustomiseDice(newAbility);
+    }
+    public void ReRollAbility()
+    {
+        RandomiseAbility();
+        Debug.Log("Hello");
     }
 }
