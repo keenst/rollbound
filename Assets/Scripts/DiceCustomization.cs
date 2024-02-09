8e20777f6b18202501f31b7db7bde5bb18adb162
Cornelis Malmros <122611844+Vengefulship136@users.noreply.github.com>
2024-02-09 09:29:17 +0100

    Problems fixed
    Fixar Felix has arrived

--- Assets/Scripts/DiceCustomization.cs
+++ Assets/Scripts/DiceCustomization.cs
@@ -7,7 +7,6 @@ using UnityEngine.UI;
 public class DiceCustomization : MonoBehaviour
 {
     private int selectedSideIndex = -1;
-    public Ability newAbility;
     public Ability currentAbility;
 
     void Start()
@@ -44,22 +43,22 @@ public class DiceCustomization : MonoBehaviour
         
     }
 
-    public void CustomiseDice(Ability newAbility, int selectedSideIndex)
+    public void CustomiseDice(Ability newAbility)
     {
         if (newAbility is PhysicalAbility)
         {
-            ChangeColor(Color.red);
-            ChangeText("Physical ability");
+            ChangeColor1(Color.red);
+            ChangeText1("Physical ability");
         }
         if (newAbility is MagicalAbility)
         {
-            ChangeColor(Color.blue);
-            ChangeText("Magical ability");
+            ChangeColor1(Color.blue);
+            ChangeText1("Magical ability");
         }
         if (newAbility is DefensiveAbility)
         {
-            ChangeColor(Color.green);
-            ChangeText("Defensive ability");
+            ChangeColor1(Color.green);
+            ChangeText1("Defensive ability");
         }
     }
     public void newAbilityCTM(Ability currentAbility)
@@ -83,12 +82,18 @@ public class DiceCustomization : MonoBehaviour
     private void ChangeColor(Color color)
     {
         GameObject.Find("New ability").GetComponent<Image>().color = color;
-        GameObject.Find("Side " + selectedSideIndex).GetComponent<Image>().color = color;
-
     }
     private void ChangeText(string text)
     {
         GameObject.Find("New ability").GetComponentInChildren<Text>().text = text;
+    }
+    private void ChangeColor1(Color color)
+    {
+        GameObject.Find("Side " + selectedSideIndex).GetComponent<Image>().color = color;
+    }
+    private void ChangeText1(string text)
+    {
+        GameObject.Find("Side " + selectedSideIndex).GetComponentInChildren<Text>().text = text;
     }
     public void RandomiseAbility()
     {
@@ -104,10 +109,9 @@ public class DiceCustomization : MonoBehaviour
     public void confirmation()
     {            
         if (selectedSideIndex != -1)
-        {        
-            Ability newAbility = currentAbility;
-            CustomiseDice(newAbility, selectedSideIndex);
-            Debug.Log("Ability on side " + selectedSideIndex + " updated to " + newAbility);
+        { 
+            CustomiseDice(currentAbility);
+            Debug.Log("Ability on side " + selectedSideIndex + " updated to " + currentAbility);
             selectedSideIndex = -1;            
             RandomiseAbility();            
         }
