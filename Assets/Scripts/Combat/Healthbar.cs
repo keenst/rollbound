7661ab90f54c94f4a84a1a78ccd84577911dd699
keenst <leo.siven@icloud.com>
2024-02-09 10:15:36 +0100

    Fixed healthbar

--- Assets/Scripts/Combat/Healthbar.cs
+++ Assets/Scripts/Combat/Healthbar.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System;
 
 public class Healthbar : MonoBehaviour
 {
@@ -6,11 +7,23 @@ public class Healthbar : MonoBehaviour
 
 	private const float MaxLength = 340;
 
+	private float startingX;
+	private int sign;
+
+	public void Start()
+	{
+		startingX = health.transform.position.x;
+		sign = Math.Sign(health.transform.parent.localPosition.x);
+	}
+
 	public void UpdateHealth(float hp, float maxHp)
 	{
-		float fillAmount = MaxLength / (hp / maxHp);
+		float fillAmount = MaxLength * (hp / maxHp);
 
 		Vector3 scale = health.transform.localScale;
-		scale = new Vector3(fillAmount, scale.y, scale.z);
+		health.transform.localScale = new Vector3(fillAmount, scale.y, scale.z);
+
+		Vector3 position = health.transform.position;
+		health.transform.position = new Vector3(startingX + sign * (MaxLength - fillAmount) / 2, position.y, position.z);
 	}
 }
