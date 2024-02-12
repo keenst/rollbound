4877a4324843b6f025c0133d4197eb29e0492b8d
Sleeperdog <95725023+Sleeper-Dog@users.noreply.github.com>
2024-02-12 13:48:06 +0100

    Loolll

--- Assets/Scripts/ShopReward/ShopButton.cs
+++ Assets/Scripts/ShopReward/ShopButton.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.Antlr3.Runtime.Tree;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -13,13 +14,21 @@ public class ShopButton : MonoBehaviour
     void Start()
     {
         controll = controller.GetComponent<ShopController>();
-        Button btn = button.GetComponent<Button>();
-        btn.onClick.AddListener(TaskOnClick);
+
+        button.onClick.AddListener(TaskOnClick);
     }
 
     void TaskOnClick()
     {
+        if (isSelected)
+        {
 
+        }
+        else
+        {
+            controller.MoveHand(button.transform);
+            //move hand based on location of ability
+        }
     }
 
 }
